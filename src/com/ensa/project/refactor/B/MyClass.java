5943e207550143fb63bb3a4ac9adfb36a667abd5
sha3loul <zarhloulyasser@gmail.com>
2023-10-18 16:49:10 +0100

    to refactor

---
+++ src/com/ensa/project/refactor/B/MyClass.java
@@ -0,0 +1,18 @@
+package com.ensa.project.refactor.B;
+
+public class MyClass {
+
+    public void function1(String s) {
+
+        // Logic to hire an employee
+        // ...
+        System.out.println("The employee " + s + " has been hired");
+    }
+
+    public void function2(String s) {
+
+        // Logic to fire an employee
+        // ...
+        System.out.println("The employee " + s + " has been fired");
+    }
+}
